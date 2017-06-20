449cf64ae8f3aca1905134af87cdee4c031275dc
nakarin0528 <n1510121@edu.cc.uec.ac.jp>
2017-06-20 16:05:27 +0900

    oneDevice Modeを追加中

---
+++ TabbedFC/SelectModeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SelectModeViewController.swift
+//  
+//
+//  Created by I,N on 2017/06/20.
+//
+//
+
+import UIKit
+
+class SelectModeViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
